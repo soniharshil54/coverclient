24b9fc2f9f6db7f13bf3bda4368d067d2a4f9b8b
vipin224 <sharshil43@yahoo.com>
2020-01-22 18:25:48 +0530

    keychain list

---
+++ js/keychaintype.js
@@ -0,0 +1,262 @@
+ $(document).ready(function(){
+  $('#add_keychain_type').click(function(e) {
+    console.log("add_keychain_type")
+    //let addpropara = globalProduct
+    let kt_name = document.getElementById("kt_name").value
+
+    //let categories = ["phonecase", "keychain"]
+  
+    let keydata = {
+      kt_name
+    }
+    console.log(keydata)
+    fetch(`http://${hosturl}:5600/api/keychain/addtype`,
+{
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: "POST",
+    body: JSON.stringify(keydata)
+})
+.then(function(res){ 
+  return res.json()
+  })
+.then(function(result){
+  console.log(result)
+  let ktid = result._id
+  console.log(result._id)
+  //getordersdatatable()
+    document.getElementById("kt_name").value = ""
+    // document.getElementById("offer_success_id").innerHTML = "Offer Created Successfully"
+      
+   
+   uploadFile(ktid)
+  
+})
+.catch(function(res){ console.log(res) })
+        });
+})
+
+//   $("#incdisdiv").click(function (evt) {
+//     $(this).hide()
+//     document.getElementById("k_categories_inc").disabled = false
+// });
+  function removeFromArray(original, remove) {
+  return original.filter(value => !remove.includes(value));
+}
+
+
+  function logOutUser(){
+    localStorage.clear();
+    window.location  =  "login.html";
+  }
+
+
+    function editkeychaintypemodal(test){
+     // console.log(test)
+
+     // console.log( $('#modal_categories_inc').val() )
+     let arrayn = test.getAttribute("data-key")
+     console.log("arrayn",arrayn)
+      //console.log(okchains[arrayn])
+      //console.log(okchains[arrayn].name)
+      //document.getElementById("modal_offer_id").value = arrayn
+
+      fetch(`http://${hosturl}:5600/api/keychain/getktbyidadmin/${arrayn}`)
+      .then(response => {
+     // console.log(response)
+     return response.json()})
+      .then(data => {
+        console.log(data)
+      document.getElementById("edit_kt_id").value = data._id
+      document.getElementById("edit_kt_name").value = data.name
+      let sliderimage = data.slider_image
+      document.getElementById("edit_kt_slider_image").src = `http://${hosturl}:5600/admin/uploads/${sliderimage}`
+    
+       $("#myModal").modal('show')
+
+      })
+     
+      .catch(err => console.log(err))
+      //console.log(document.getElementById("modal_id").value)
+}
+
+function openimagemodal(imgpath){
+  let sliderimgsource = `http://${hosturl}:5600/admin/uploads/${imgpath}`
+  document.getElementById("imgModal").src = sliderimgsource
+  $("#imageModal").modal('show')
+}
+
+
+    function editkeychaintype(){
+      let kt_id = document.getElementById("edit_kt_id").value
+    let kt_name = document.getElementById("edit_kt_name").value
+     let ktdata = {
+      name : kt_name
+    }
+      // console.log(keyid)
+      console.log("k edit data")
+      // console.log(keditdata)
+
+          fetch(`http://${hosturl}:5600/api/keychain/editkeychaintype/${kt_id}`,
+          {
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: "PUT",
+            body: JSON.stringify(ktdata)
+          })
+          .then(function(res){ 
+            uploadFileEdit(kt_id)
+            //getoffers()
+            $("#myModal").modal("hide");
+ 
+          })
+          .catch(function(res){ console.log(res) })
+    }
+
+
+  function uploadFile(ktid){
+    console.log("uploads file")
+    //var input = document.querySelector('input[type="file"]')
+    var form = document.getElementById("keychainTypeImage");
+    var formData = new FormData(form);
+    console.log(formData)
+
+fetch(`http://${hosturl}:5600/api/keychain/ktaddimage/${ktid}`, {
+  method: 'PUT',
+  body: formData
+}).then(res => {
+  console.log(res)
+  //document.getElementById("successAdded").innerHTML = "Offer successfully added !!!"
+ // window.location = "offerlist.html"
+ // getoffers()
+}).catch(err => console.log(err))
+
+  }
+
+
+    function uploadFileEdit(ktid){
+    console.log("uploads file")
+    //var input = document.querySelector('input[type="file"]')
+    var form = document.getElementById("ktImageEdit");
+    var formData = new FormData(form);
+    console.log(formData)
+
+fetch(`http://${hosturl}:5600/api/keychain/ktaddimage/${ktid}`, {
+  method: 'PUT',
+  body: formData
+}).then(res => {
+  console.log(res)
+  //document.getElementById("successAdded").innerHTML = "Offer successfully added !!!"
+ // window.location = "offerlist.html"
+ // getoffers()
+}).catch(err => console.log(err))
+
+  }
+
+
+  function enableit(selectin){
+    console.log("chnage")
+    selectin.disabled = false
+  }
+
+  function disablecat(selectele){
+    console.log(selectele.id)
+    console.log("clicked")
+    let incsel = document.getElementById("k_categories_inc")
+    let exsel = document.getElementById("k_categories_ex")
+    if(selectele.id === "k_categories_inc"){
+      if(selectele.value === "" || selectele.value === null){
+        exsel.disabled = false
+      }
+      else{
+        exsel.disabled = true
+      }
+    }
+    else if(selectele.id === "k_categories_ex") {
+      if(selectele.value === "" || selectele.value === null){
+        incsel.disabled = false
+      }
+      else{
+        incsel.disabled = true
+      }
+    }
+  }
+
+        function getktypesdatatable(){
+          let userTable = $('#example1').DataTable({
+            destroy: true,
+        "processing" : true,
+        "aaSorting": [[ 4, "desc" ]],
+          "rowCallback": function (nRow, aData, iDisplayIndex) {
+               var oSettings = this.fnSettings ();
+               $("td:first", nRow).html(oSettings._iDisplayStart+iDisplayIndex +1);
+               return nRow;
+          },
+        "ajax" : {
+            "url" : `http://${hosturl}:5600/api/keychain/getallkeychaintypes`,
+            dataSrc : ''
+        },
+        "columns" : [ {
+            "data" : null
+        }, {
+            "data" : "name"
+            
+        }, {
+            "data" : "slider_image",
+               "mRender": function(data, type) {
+             //return data
+              return `<button onclick="openimagemodal('${data}')" style="padding: 1px 1px; margin:5px" class="btn btn-info" data-pid="${data}">view</button>`;
+            }
+        },{
+            "data" : "create_date",
+            "visible":false
+        }, {
+          "data": null,
+            "mRender": function(data, type) {
+             //return data
+             let outerbutton = ""
+             let statuslink = ""
+             if(data.active_status === 1){
+
+            outerbutton = `<button type="button" style="margin:5px" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown">Active</button>`
+            statuslink = `<a data-key=${data._id} onclick="deactiveoffer(this)" class="dropdown-item">Inactive</a>`
+             }
+              else {
+             outerbutton = `<button type="button" style="margin:5px"  class="btn btn-danger btn-sm dropdown-toggle" data-toggle="dropdown">Deactivated</button>`
+             statuslink = `<a data-key=${data._id} onclick="activeoffer(this)"  class="dropdown-item">Active</a>`
+             }
+
+              return `<div class="dropdown">${outerbutton}<div class="dropdown-menu">${statuslink}<a onclick="editkeychaintypemodal(this)" data-key="${data._id}" class="dropdown-item">Edit</a></div></div>`;
+            }
+        }, {
+          "data": "_id",
+            "mRender": function(data, type) {
+             
+              return `<input name="todelete" value=${data} type="checkbox">`;
+            }
+        }]
+      });
+    }
+
+    getktypesdatatable()
+
+          
+    
+
+        function checkLogin(){
+    
+    if (localStorage.getItem("luser") === null) {
+        window.location = "login.html";
+    }
+    else{
+      console.log(localStorage.getItem("luser"))
+
+      var authtokend = localStorage.getItem('authorization')
+      console.log(authtokend)  
+       }
+  }
+  checkLogin()

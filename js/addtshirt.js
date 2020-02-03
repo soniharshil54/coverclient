320f75b3ccd133a166d19c933a75fcd3033c4573
vipin224 <sharshil43@yahoo.com>
2020-02-03 18:13:53 +0530

    watch edits

--- js/addtshirt.js
+++ js/addtshirt.js
@@ -59,6 +59,31 @@
         });
 })
 
+ $(function() {
+    // Multiple images preview in browser
+    var imagesPreview = function(input, placeToInsertImagePreview) {
+
+        if (input.files) {
+            var filesAmount = input.files.length;
+
+            for (i = 0; i < filesAmount; i++) {
+                var reader = new FileReader();
+
+                reader.onload = function(event) {
+                    $($.parseHTML('<img>')).attr('src', event.target.result).attr('width', '50px').appendTo(placeToInsertImagePreview);
+                }
+
+                reader.readAsDataURL(input.files[i]);
+            }
+        }
+
+    };
+
+    $('#tshirtImages').on('change', function() {
+        imagesPreview(this, 'div.gallery');
+    });
+});
+
 //   $("#incdisdiv").click(function (evt) {
 //     $(this).hide()
 //     document.getElementById("k_categories_inc").disabled = false

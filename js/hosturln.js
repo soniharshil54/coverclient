cf0c3cd2f4629264d9b3eb3871585d148609b294
harshil <sharshil43@yahoo.com>
2020-03-15 17:12:33 +0530

    price validation

--- js/hosturln.js
+++ js/hosturln.js
@@ -2,6 +2,12 @@
 
 hosturl = "95.216.71.108"
 
+ $('input[Type="Number"]').keypress(function (e) {
+    if ('0123456789'.indexOf(e.key) == -1) {
+        e.preventDefault();
+    }
+});
+
 
 // hosturl = "localhost"
 
